57f7e7c15bc1f89a2386c7a168ae1a6a8026065b
lpalacio26 <lpalaciosuttor@gmail.com>
2016-12-14 16:35:22 +0100

    criteria.finder_skeleton
    Now does the stuff it was supposed to (iterates over dictionary with musicians and selects them according to criteria selected)

--- criteria-finder.py
+++ criteria-finder.py
@@ -1,3 +1,6 @@
+musicians = {}
+
+
 class Musician:
 
     def __init__(self, name, gender, bands, genre, instruments):
@@ -45,16 +48,59 @@ def musician_creator():
     instruments += add('instruments')
     genres += add('genres')
 
-    return Musician(name, gender, bands, genres, instruments)
+    musicians[name] = Musician(name, gender, bands, genres, instruments)
 
 
-x = musician_creator()
+def show_musician(musician):
 
-print(x.bands)
+    print('NAME: ' + musician.name)
+    print('GENDER: ' + musician.gender)
+    bands = ""
+    for i in musician.bands:
+        bands += i + ' - '
+    print('BANDS: ' + bands)
+
+    instruments = ""
+    for i in musician.instruments:
+        instruments += i + ' - '
+    print('INSTRUMENTS: ' + instruments)
+
+    genres = ""
+    for i in musician.genre:
+        genres += i + ' - '
+    print('GENRES: ' + genres)
+
+Lorenzo = Musician('Lorenzo', 'Male', ['Radiohead'], ['Jazz'], ['Bass'])
+musicians['Lorenzo'] = Lorenzo
 
 
+def criteria_finder(instruments, genre, bands, gender, musician):
+
+        for elem in musician:
+
+            finder = True
+
+            if instruments != "NA":
+                for i in instruments:
+                        if i not in musician[elem].instruments:
+                            finder = False
+
+            if bands != ["NA"]:
+                for i in bands:
+                        if i not in musician[elem].bands:
+                            finder = False
+
+            if genre != ["NA"]:
+                for i in genre:
+                        if i not in musician[elem].genre:
+                            finder = False
+
+            if gender != "NA":
+                if gender != musician[elem].gender:
+                    finder = False
+
+            if finder:
+                show_musician(musicians[elem])
 
 
-
-
-
+criteria_finder(['Bass'], ['NA'], ['Radiohead'], 'NA', musicians)
